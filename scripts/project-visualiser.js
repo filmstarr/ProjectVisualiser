049324609c37a9b43f73278fccc5e6ce4eecf571
Ross Huelin <filmstarr@hotmail.com>
2016-02-03 15:26:46 +0000

    Project updates.

--- scripts/project-visualiser.js
+++ scripts/project-visualiser.js
@@ -1,32 +1,45 @@
+//Data
+var states = Object.freeze({Backlog: 0, Prioritised: 1, Working: 2, Testing: 3, Complete: 4, Released: 5});
+var projects = [
+  {Name: "Project A", Description: "", StartDate: new Date(2016,6-1,1), TestDays: 28, EndDate: new Date(2016,12-1,1), Effort: 1.0, StateProgress: 1.0, State: states.Prioritised, Archive: false, Details: ""},
+  {Name: "Project B", Description: "", StartDate: new Date(2016,9-1,1), TestDays: 14, EndDate: new Date(2016,11-1,1), Effort: 0.3, StateProgress: 1.0, State: states.Prioritised, Archive: false, Details: ""},
+  {Name: "Project C", Description: "", StartDate: new Date(2016,9-1,1), TestDays: 7, EndDate: new Date(2016,12-1,1), Effort: 0.6, StateProgress: 1.0, State: states.Prioritised, Archive: false, Details: ""},
+  {Name: "Project D", Description: "", StartDate: new Date(2016,4-1,1), TestDays: 14, EndDate: new Date(2016,6-1,1), Effort: 0.5, StateProgress: 1.0, State: states.Prioritised, Archive: false, Details: ""},
+  {Name: "Project E", Description: "", StartDate: new Date(2016,6-1,1), TestDays: 21, EndDate: new Date(2016,8-1,1), Effort: 0.8, StateProgress: 1.0, State: states.Prioritised, Archive: false, Details: ""},
+  {Name: "Project F", Description: "", StartDate: new Date(2016,12-1,1), TestDays: 21, EndDate: new Date(2017,02-1,1), Effort: 0.4, StateProgress: 1.0, State: states.Prioritised, Archive: false, Details: ""},
+  {Name: "Project G", Description: "", StartDate: new Date(2016,1-1,1), TestDays: 14, EndDate: new Date(2016,4-1,15), Effort: 1.0, StateProgress: 0.4, State: states.Working, Archive: false, Details: ""},
+  {Name: "Project H", Description: "", StartDate: new Date(2015,12-1,1), TestDays: 7, EndDate: new Date(2016,2-1,29), Effort: 0.7, StateProgress: 0.3, State: states.Working, Archive: false, Details: ""},
+  {Name: "Project I", Description: "", StartDate: new Date(2016,2-1,1), TestDays: 7, EndDate: new Date(2016,2-1,1), Effort: 0.3, StateProgress: 0.3, State: states.Working, Archive: false, Details: ""},
+  {Name: "Project J", Description: "", StartDate: new Date(2016,2-1,1), TestDays: 7, EndDate: new Date(2016,4-1,29), Effort: 0.3, StateProgress: 0.2, State: states.Working, Archive: false, Details: ""},
+  {Name: "Project K", Description: "", StartDate: new Date(2016,1-1,21), TestDays: 14, EndDate: new Date(2016,4-1,1), Effort: 0.7, StateProgress: 0.5, State: states.Working, Archive: false, Details: ""},
+  {Name: "Project L", Description: "", StartDate: new Date(2016,1-14,1), TestDays: 2, EndDate: new Date(2016,3-1,1), Effort: 0.2, StateProgress: 1.0, State: states.Testing, Archive: false, Details: ""},
+  {Name: "Project M", Description: "", StartDate: new Date(2016,1-1,1), TestDays: 1, EndDate: new Date(2016,2-1,1), Effort: 0.4, StateProgress: 1.0, State: states.Released, Archive: false, Details: ""},
+];
+
+projects = projects.sort(function(a, b) {
+  if (a.Name < b.Name)
+    return -1;
+  else if (a.Name > b.Name)
+    return 1;
+  else 
+    return 0;
+});
+
 //Define parameters
 var xSize = paper.view.bounds.width;
 var ySize = paper.view.bounds.height-20;
 var centre = new Point(xSize/2, ySize);
-var totalRadius = (ySize/1.2);
+var totalRadius = Math.min((ySize/1.2),(xSize/2))-1;
 var totalProjectAngle = 110;
 var projectArcLineFactor = 1.03;
-
-//States
-var states = Object.freeze({Backlog: 0, Prioritised: 1, Working: 2, Testing: 3, Complete: 4, Released: 5});
 var stateLines =
 [
-  {State: states["Backlog"], Lines: 1},
-  {State: states["Prioritised"], Lines: 1},
-  {State: states["Working"], Lines: 10},
-  {State: states["Testing"], Lines: 1},
-  {State: states["Complete"], Lines: 1},
-  {State: states["Released"], Lines: 1},
-];
-
-//Project list
-var projects =
-[
-  {Name: "Project A", StartDate: new Date(2016,1-1,24), TestDays: 5, EndDate: new Date(2016,4-1,24), Effort: 1.0, StateProgress: 0.5, State: states["Backlog"], Archive: false, Details: ""},
-  {Name: "Project B", StartDate: new Date(2016,1-1,24), TestDays: 5, EndDate: new Date(2016,4-1,24), Effort: 0.4, StateProgress: 0.5, State: states["Prioritised"], Archive: false, Details: ""},
-  {Name: "Project C", StartDate: new Date(2016,1-1,01), TestDays: 5, EndDate: new Date(2016,2-1,28), Effort: 0.1, StateProgress: 0.7, State: states["Working"], Archive: false, Details: ""},
-  {Name: "Project D", StartDate: new Date(2016,1-1,24), TestDays: 25, EndDate: new Date(2016,2-1,24), Effort: 0.8, StateProgress: 0.5, State: states["Testing"], Archive: false, Details: ""},
-  {Name: "Project E", StartDate: new Date(2016,1-1,24), TestDays: 5, EndDate: new Date(2016,1-1,24), Effort: 0.2, StateProgress: 0.5, State: states["Complete"], Archive: false, Details: ""},
-  {Name: "Project F", StartDate: new Date(2016,1-1,24), TestDays: 5, EndDate: new Date(2016,2-1,24), Effort: 0.5, StateProgress: 0.5, State: states["Released"], Archive: true, Details: ""},
+  {State: states.Backlog, Lines: 1},
+  {State: states.Prioritised, Lines: 1},
+  {State: states.Working, Lines: 10},
+  {State: states.Testing, Lines: 1},
+  {State: states.Complete, Lines: 1},
+  {State: states.Released, Lines: 1},
 ];
 
 
@@ -34,7 +47,7 @@ var projects =
 var innerSegmentLines = 5;
 var totalLines = 0;
 for(var i=0; i<stateLines.length; i++ ) {
-  totalLines += stateLines[i]["Lines"]+1;
+  totalLines += stateLines[i].Lines+1;
 }
 totalLines += innerSegmentLines;
 var lineSpacing = totalRadius/totalLines;
@@ -46,8 +59,8 @@ ArcLine(centre, totalRadius, (totalProjectAngle/2)+100, false, [new Color(56/255
 var remainingLines = totalLines;
 for(var i=stateLines.length-1; i >= 0; i-- ) {
   var index = stateLines.length-1-i;
-  var lineCount = stateLines[index]["Lines"];
-  var state = Object.keys(states)[stateLines[index]["State"]];
+  var lineCount = stateLines[index].Lines;
+  var state = Object.keys(states)[stateLines[index].State];
   State(centre, remainingLines*lineSpacing, (remainingLines-lineCount-1)*lineSpacing, totalProjectAngle + 7.5+((i-1)*5), [new Color(186/255, 178/255, 177/255, 0.70-(i*0.12))], i*2.5, lineCount, state, totalProjectAngle);
   remainingLines -= lineCount+1;
 }
@@ -55,7 +68,7 @@ for(var i=stateLines.length-1; i >= 0; i-- ) {
 //Projects
 var projectCount = 0;
 for(var i=0; i < projects.length; i++ ) {
-  if (!projects[i]["Archive"]) {
+  if (!projects[i].Archive) {
     projectCount++;
   }
 }
@@ -66,33 +79,26 @@ var projectColour = [new Color(32/255, 0/255, 0/255, 1.0)];
 //Project arcline
 ArcLine(centre, totalRadius*projectArcLineFactor, (totalProjectAngle/2) + 90 - projectOffsetAngle, false, [new Color(87/255, 55/255, 105/255, 1.0)], totalProjectAngle, 90-(totalProjectAngle/2));
 var circle = new Path.Circle(new Point(centre.x + totalRadius*projectArcLineFactor, centre.y), 4);
-circle.fillColor = 'white';
+circle.fillColor = "white";
 circle.strokeColor = [new Color(87/255, 55/255, 105/255, 1.0)];
 
-//Background project lines and stuff
+//Background project lines
 for(var i=0; i < projects.length; i++ ) {
   var project = projects[i];
-  if (!project["Archive"]) {
+  if (!project.Archive) {
     //Line
     var line = new Path.Line(centre, new Point(centre.x, centre.y-totalRadius+1));
     line.strokeColor = new Color(255/255, 255/255, 255/255, 0.4);
-
-    //Project circle
-    var circle = new Path.Circle(new Point(centre.x, centre.y - (totalRadius*projectArcLineFactor)), 4);
-    circle.fillColor = projectColour;
-
-    //Rotation
-    var group = new Group(line, circle);
-    group.rotate(-(totalProjectAngle/2)+projectOffsetAngle+(projectSeparationAngle*i), centre);       
+    line.rotate(-(totalProjectAngle/2)+projectOffsetAngle+(projectSeparationAngle*i), centre);       
   }
 }
 
 //Project details
-var projectColours = ["#FA37A8","#23b3e8","#d5547f","#9260a7"];
+var projectColours = ["#FA37A8","#23B3E8","#D5547F","#00E957","#9260A7"];
 for(var i=0; i < projects.length; i++ ) {
   var project = projects[i];
-  if (!project["Archive"]) {
-    var project = new Project(centre, totalRadius, project, stateLines, lineSpacing, projectCount, new Color(projectColours[i % projectColours.length ]), totalProjectAngle, projectOffsetAngle, projectSeparationAngle);
+  if (!project.Archive) {
+    var project = new Project(centre, totalRadius, project, stateLines, lineSpacing, projectCount, new Color(projectColours[i % projectColours.length ]), totalProjectAngle, projectOffsetAngle, projectSeparationAngle, states);
   }
 }
 
@@ -105,6 +111,8 @@ Doughnut(centre, innerSegmentWidth*0.12, innerSegmentWidth*0.02, [new Color(0/25
 
 //TODO: Show project details in side bar when clicked on
 //TODO: Archived items
+
+
 
 //Objects
 function State(centre, radius, innerRadius, angle, colour, angleOffset, lineCount, state, totalProjectAngle){
@@ -141,7 +149,7 @@ function State(centre, radius, innerRadius, angle, colour, angleOffset, lineCoun
 
   //State name
   var text = new PointText(new Point(centre.x-8, centre.y+3-radius+((radius-innerRadius)/2)));
-  text.justification = 'right';
+  text.justification = "right";
   text.fillColor = new Color(32/255, 0/255, 0/255, 1.0);
   text.content = state;
   var background = new Path.Rectangle(text.bounds);
@@ -155,58 +163,70 @@ function State(centre, radius, innerRadius, angle, colour, angleOffset, lineCoun
   return group.addChild(textGroup);
 }
 
-function Project(centre, totalRadius, project, stateLines, lineSpacing, projectCount, projectColour, totalProjectAngle, projectOffsetAngle, projectSeparationAngle){
+function Project(centre, totalRadius, project, stateLines, lineSpacing, projectCount, projectColour, totalProjectAngle, projectOffsetAngle, projectSeparationAngle, states){
     //Project title
     var text = new PointText(new Point(centre.x, centre.y - (totalRadius*1.06)));
-    text.justification = 'center';
+    text.justification = "center";
     text.fillColor = projectColour;
-    text.content = project["Name"];
+    text.content = project.Name;
 
     //State
-    var projectState = project["State"];
+    var projectState = project.State;
     var projectLines = -1;
     for(var j=0; j<stateLines.length; j++ ) {
       var stateLine = stateLines[j];
-      if (projectState == stateLine["State"])
+      if (projectState == stateLine.State)
       {
-        projectLines += Math.round(((stateLine["Lines"]-1)*project["StateProgress"])+2);
+        projectLines += Math.round(((stateLine.Lines-1)*project.StateProgress)+2);
         break;
       }
-      projectLines += stateLine["Lines"]+1;
+      projectLines += stateLine.Lines+1;
     }
 
+    //Project circle
+    var circle = new Path.Circle(new Point(centre.x, centre.y - (totalRadius*projectArcLineFactor)), 4);
+    circle.fillColor = projectColour;
+
     //Project bubble
-    var outerRadius = 25*projectCount*project["Effort"];
+    var outerRadius = 1.5*(totalRadius/projectCount)*project.Effort;
     var bubble = new Doughnut(new Point(centre.x, centre.y - totalRadius + (lineSpacing*projectLines)), outerRadius, 2, projectColour);
     bubble.fillColor.alpha = 0.8;
 
     //Project status
-    var now = new Date().getTime();
-    var testingStartDate = project["EndDate"].getTime()-(project["TestDays"] * 86400000);
-    var workingTime = testingStartDate - project["StartDate"].getTime();
-    var elapsedTime = now - project["StartDate"].getTime();
-    if (projectState < states["Working"] && project["StartDate"] < now) {
-      project["Details"] = "Behind schedule - Not working";
-    }
-    if (project["StateProgress"] < elapsedTime/workingTime && projectState == states["Working"]) {
-      project["Details"] = "Behind schedule - Not progressed enough";
-    }
-    if (testingStartDate < now && projectState < states["Testing"]) {
-      project["Details"] = "Behind schedule - Not testing yet";
-    }
-    if (project["EndDate"] < now && projectState < states["Complete"]) {
-      project["Details"] = "Behind schedule - Not complete yet";
-    }
-
+    UpdateStatus(project)
     var statusBubble = new Doughnut(new Point(centre.x, centre.y - totalRadius + (lineSpacing*projectLines)), outerRadius+4, outerRadius+2, [new Color(72/255, 186/255, 60/255, 0.8)]);
-    if (project["Details"] != "") {
+    if (project.Details != "") {
       statusBubble.fillColor = [new Color(238/255, 28/255, 36/255, 0.8)];
     }
-
 
     //Rotation
     var group = new Group(circle, text, bubble, statusBubble);
     group.rotate(-(totalProjectAngle/2)+projectOffsetAngle+(projectSeparationAngle*i), centre);
+  
+    //Events
+    group.onClick = function(event) {
+      console.log(project);
+    }
+    
+    function UpdateStatus(project) {
+      var projectState = project.State;
+      var now = new Date().getTime();
+      var testingStartDate = project.EndDate.getTime()-(project.TestDays * 86400000);
+      var workingTime = testingStartDate - project.StartDate.getTime();
+      var elapsedTime = now - project.StartDate.getTime();
+      if (projectState < states.Working && project.StartDate < now) {
+        project.Details = "Behind schedule - Not working";
+      }
+      if (project.StateProgress < elapsedTime/workingTime && projectState == states.Working) {
+        project.Details = "Behind schedule - Not progressed enough";
+      }
+      if (testingStartDate < now && projectState < states.Testing) {
+        project.Details = "Behind schedule - Not testing yet";
+      }
+      if (project.EndDate < now && projectState < states.Complete) {
+        project.Details = "Behind schedule - Not complete yet";
+      }
+    }
 }
 
 function ArcLine(centre, radius, angle, beginningCircle, colour, totalProjectAngle, angleOffset){
